cd0f2089e8c8c14d74cb8a3e29fe2890b660925c
dougal <dougal.s@gmail.com>
2008-11-07 10:48:21 -0700

    Changed the way components are required to allow them to run in both tests and as a plugin.

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
-require 'lib/acts_as_follower'
-require 'lib/acts_as_followable'
-require 'lib/follow'
+require File.dirname(__FILE__) + '/lib/acts_as_follower'
+require File.dirname(__FILE__) + '/lib/acts_as_followable'
+require File.dirname(__FILE__) + '/lib/follow'
 
 ActiveRecord::Base.send(:include, ActiveRecord::Acts::Follower)
 ActiveRecord::Base.send(:include, ActiveRecord::Acts::Followable)
