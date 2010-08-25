a8a37833555f33f1f4242c440872c201c4bbea07
tcocca <tom.cocca@gmail.com>
2010-08-25 07:07:45 -0400

    created a rails 3 branch of the plugin

--- init.rb
+++ init.rb
@@ -1,5 +1,1 @@
-require File.dirname(__FILE__) + '/lib/acts_as_follower'
-require File.dirname(__FILE__) + '/lib/acts_as_followable'
-
-ActiveRecord::Base.send(:include, ActiveRecord::Acts::Follower)
-ActiveRecord::Base.send(:include, ActiveRecord::Acts::Followable)
+require 'acts_as_follower'
