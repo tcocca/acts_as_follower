72bca0bf06ed226fc658f60a2140af21a622cc91
tcocca <tom.cocca@gmail.com>
2010-09-25 18:12:57 -0400

    updated the library to take advantage of AREL and the new AR3 query interface, make Follow include the FollowerLib as a class method to allow for use in the scopes, allows for a drastic cleanup of the code.  Finally, following_by_type and followers_by_type now return collections instead of arrays to allow these methods to be extended upon

--- lib/generators/templates/model.rb
+++ lib/generators/templates/model.rb
@@ -1,19 +1,21 @@
 class Follow < ActiveRecord::Base
-
-  scope :for_follower,    lambda { |*args| {:conditions => ["follower_id = ? AND follower_type = ?", args.first.id, args.first.type.name]} }
-  scope :for_followable, lambda { |*args| {:conditions => ["followable_id = ? AND followable_type = ?", args.first.id, args.first.type.name]} }
-  scope :recent,       lambda { |*args| {:conditions => ["created_at > ?", (args.first || 2.weeks.ago).to_s(:db)]} }
-  scope :descending, :order => "created_at DESC"
-  scope :unblocked, :conditions => {:blocked => false}
-  scope :blocked, :conditions => {:blocked => true}
-
+  extend ActsAsFollower::FollowerLib
+  
+  scope :for_follower,        lambda { |follower| where(["follower_id = ? AND follower_type = ?", follower.id, parent_class_name(follower)]) }
+  scope :for_followable,      lambda { |followable| where(["followable_id = ? AND followable_type = ?", followable.id, parent_class_name(followable)]) }
+  scope :for_follower_type,   lambda { |follower_type| where("follower_type = ?", follower_type) }
+  scope :for_followable_type, lambda { |followable_type| where("followable_type = ?", followable_type) }
+  scope :recent,              lambda { |from| where(["created_at > ?", (from || 2.weeks.ago).to_s(:db)]) }
+  scope :descending,          order("follows.created_at DESC")
+  scope :unblocked,           where(:blocked => false)
+  scope :blocked,             where(:blocked => true)
+  
   # NOTE: Follows belong to the "followable" interface, and also to followers
   belongs_to :followable, :polymorphic => true
   belongs_to :follower,   :polymorphic => true
-
+  
   def block!
     self.update_attribute(:blocked, true)
   end
-
+  
 end
-
