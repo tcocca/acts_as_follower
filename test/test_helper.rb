6dadd9798094c45d810ee36a91a5f5f13cef05de
James McCarthy <james2mccarthy@gmail.com>
2011-09-20 23:53:26 +0100

    Changed test environment to have embedded rails and only use sqlite3.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,20 +1,19 @@
-require 'rubygems'
-require 'logger'
-require 'active_record'
+# Configure Rails Envinronment
+ENV["RAILS_ENV"] = "test"
+
+require File.expand_path("../dummy30/config/environment.rb",  __FILE__)
+require "rails/test_help"
 
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
-ActiveRecord::Base.configurations = YAML::load(File.open(File.dirname(__FILE__) + '/database.yml'))
-ActiveRecord::Base.establish_connection(ENV['DB'] || 'mysql')
+ActiveRecord::Migration.verbose = false
 
-require File.dirname(__FILE__) + '/../init.rb'
+load(File.dirname(__FILE__) + '/schema.rb')
+
 require File.dirname(__FILE__) + '/models/band'
 require File.dirname(__FILE__) + '/models/user'
 require File.dirname(__FILE__) + '/../lib/generators/templates/model.rb'
 
-require 'test/unit'
-require 'active_support'
 require 'shoulda'
 require 'factory_girl'
-Factory.find_definitions
+FactoryGirl.find_definitions
 
-load(File.dirname(__FILE__) + '/schema.rb')
