b5851e66f261f4dfcb4c7697529aa3090202a2b7
James McCarthy <james2mccarthy@gmail.com>
2011-09-20 23:51:43 +0100

    Added railtie.

--- lib/acts_as_follower.rb
+++ lib/acts_as_follower.rb
@@ -4,4 +4,8 @@ module ActsAsFollower
   autoload :Follower,     'acts_as_follower/follower'
   autoload :Followable,   'acts_as_follower/followable'
   autoload :FollowerLib,  'acts_as_follower/follower_lib'
+  
+  require 'acts_as_follower/railtie' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
 end
+
+
