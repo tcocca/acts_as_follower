2954a768206156e004b6306ddb697370c75068c9
James McCarthy <james2mccarthy@gmail.com>
2011-09-22 18:16:53 +0100

    Split Follow scopes out into their own module.

--- lib/acts_as_follower.rb
+++ lib/acts_as_follower.rb
@@ -4,6 +4,7 @@ module ActsAsFollower
   autoload :Follower,     'acts_as_follower/follower'
   autoload :Followable,   'acts_as_follower/followable'
   autoload :FollowerLib,  'acts_as_follower/follower_lib'
+  autoload :FollowScopes, 'acts_as_follower/follow_scopes'
   
   require 'acts_as_follower/railtie' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
 end
