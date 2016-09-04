34288b101ccf92bca80b29195607d6d6be7b0e44
Amol Udage <amoludage@joshsoftware.com>
2016-09-05 00:52:05 +0530

    Update ruby hash syntax

--- lib/acts_as_follower/follow_scopes.rb
+++ lib/acts_as_follower/follow_scopes.rb
@@ -3,23 +3,22 @@ module ActsAsFollower #:nodoc:
 
     # returns Follow records where follower is the record passed in.
     def for_follower(follower)
-      where(:follower_id => follower.id,
-            :follower_type => parent_class_name(follower))
+      where(follower_id: follower.id, follower_type: parent_class_name(follower))
     end
 
     # returns Follow records where followable is the record passed in.
     def for_followable(followable)
-      where(:followable_id => followable.id, :followable_type => parent_class_name(followable))
+      where(followable_id: followable.id, followable_type: parent_class_name(followable))
     end
 
     # returns Follow records where follower_type is the record passed in.
     def for_follower_type(follower_type)
-      where(:follower_type => follower_type)
+      where(follower_type: follower_type)
     end
 
     # returns Follow records where followeable_type is the record passed in.
     def for_followable_type(followable_type)
-      where(:followable_type => followable_type)
+      where(followable_type: followable_type)
     end
 
     # returns Follow records from past 2 weeks with default parameter.
@@ -34,12 +33,12 @@ module ActsAsFollower #:nodoc:
 
     # returns unblocked Follow records.
     def unblocked
-      where(:blocked => false)
+      where(blocked: false)
     end
 
     # returns blocked Follow records.
     def blocked
-      where(:blocked => true)
+      where(blocked: true)
     end
 
   end
