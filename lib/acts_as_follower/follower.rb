d7c6ab3ce2b38fe9797014579bc867a223580066
Ishan Kanade <ishan@amuratech.com>
2016-09-28 17:37:59 +0530

    fixed follow bug

--- lib/acts_as_follower/follower.rb
+++ lib/acts_as_follower/follower.rb
@@ -29,8 +29,7 @@ module ActsAsFollower #:nodoc:
       # Does not allow duplicate records to be created.
       def follow(followable)
         if self != followable
-          params = {followable_id: followable.id, followable_type: parent_class_name(followable)}
-          self.follows.where(params).first_or_create!
+          self.follows.find_or_create_by(followable_id: followable.id, followable_type: parent_class_name(followable))
         end
       end
 
