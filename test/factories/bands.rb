3f56402e4356c8e8ed37c398de77958de740ed23
Brian Christian <brchristian@gmail.com>
2016-12-23 21:49:48 -0800

    hash colons

--- test/factories/bands.rb
+++ test/factories/bands.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
-  factory :oasis, :class => Band do |b|
+  factory :oasis, class: Band do |b|
     b.name 'Oasis'
   end
 
-  factory :metallica, :class => Band do |b|
+  factory :metallica, class: Band do |b|
     b.name 'Metallica'
   end
   
