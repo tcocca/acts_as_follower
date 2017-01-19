d84ac4313a818addf0326ba5b850986f08955500
Brian Christian <brchristian@gmail.com>
2017-01-19 15:08:52 -0800

    restore and deprecate custom_parent_classes

--- lib/acts_as_follower/follower_lib.rb
+++ lib/acts_as_follower/follower_lib.rb
@@ -3,9 +3,14 @@ module ActsAsFollower
 
     private
 
+    DEFAULT_PARENTS = [ApplicationRecord, ActiveRecord::Base]
+
     # Retrieves the parent class name if using STI.
     def parent_class_name(obj)
-      obj.class.base_class.name
+      unless parent_classes.include?(obj.class.superclass)
+        return obj.class.base_class.name
+      end
+      obj.class.name
     end
 
     def apply_options_to_scope(scope, options = {})
@@ -27,5 +32,11 @@ module ActsAsFollower
       scope
     end
 
+    def parent_classes
+      return DEFAULT_PARENTS unless ActsAsFollower.custom_parent_classes
+
+      ActiveSupport::Deprecation.warn("Setting custom parent classes is deprecated and will be removed in future versions.")
+      ActsAsFollower.custom_parent_classes + DEFAULT_PARENTS
+    end
   end
 end
